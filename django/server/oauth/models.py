e2039afc9eeceb5dccc6d044b9712408c37ec77a
hezhi <hezhi@shengpay.com>
2018-06-20 17:38:21 +0800

    增加 oauth模型

--- django/server/oauth/models.py
+++ django/server/oauth/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from django.utils.timezone import now
+
 # Create your models here.
+
+class OAuthUser(models.Model):
+    author = models.CharField(max_length=50,verbose_name='用户')
+    nickName = models.CharField(max_length=50, verbose_name='昵称')
+    openid = models.CharField(max_length=50, verbose_name='用户')
+
+    token = models.CharField(max_length=50,verbose_name='令牌')
+    created_time = models.DateTimeField(default=now,verbose_name='创建时间')
+    last_mod_time = models.DateTimeField(default=now, verbose_name='修改时间')
+
+    def __str__(self):
+        return self.nickName
--- django/server/server/settings.py
+++ django/server/server/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'corsheaders',
+    'oauth'
 ]
 
 MIDDLEWARE = [
