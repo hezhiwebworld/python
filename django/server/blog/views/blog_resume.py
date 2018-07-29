52b3151c1de02f9a6ab0a1c2c89bcfde753e6bb6
hezhi <531999840@qq.com>
2018-07-29 18:26:34 +0800

    增加文章列表的分页

--- django/server/blog/views/blog_resume.py
+++ django/server/blog/views/blog_resume.py
@@ -4,22 +4,38 @@ from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
 
 from blog.models import Resume
-from blog.models import Power
+# from blog.models import Power
+
+
 
 @csrf_exempt  # 去掉csrf保护
 def resume(request):
-    Resume_queryset = Resume.objects.all()  # 得到queryset 对象
-    Power_queryset = Power.objects.all()  # 得到queryset 对象
-    Resume_list = []
+    # 实体
+    Resume_queryset = Resume.objects.get(name="何志")  # 得到queryset 对象
+    Power_queryset = Resume_queryset.powername.values_list('powername',flat=True)  # 得到queryset 对象
+    # value()
+    print(Power_queryset)
+    Power_list = []
+    # print(Power_queryset)
+    for art in Power_queryset:
+        Power_list.append(art)
 
-    for art in Resume_queryset:
-        # print(art.title)
-        # print(art.author)
-        # print(art.create_date)
-        # print(art.content)
-        # print(art.reading_amount)
-        temp = model_to_dict(art)  # 将一个queryset对象转化为  字典
-        Resume_list.append(temp)
-    print(Resume_list)
-    return JsonResponse({'articleList': 1})
+    temp = model_to_dict(Resume_queryset)  # 将一个queryset对象转化为  字典
+    # power_dict = model_to_dict(Power_queryset)
+    # Resume_list.append(temp)
+    print(type(Power_list[0]))
+    # print(power_dict)
+    print(Power_list)
+    # print(temp.name)
+    # for key in temp:
+    #     print(temp.key)
+    temp['powername'] = Power_list
+    del temp['id']
+
+    returnObject = {
+        'returnCode': 0,
+        'returnMsg' : "成功" ,
+        'articleList':temp
+    }
+    return JsonResponse(returnObject, safe=False)
 
